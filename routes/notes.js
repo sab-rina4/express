6aea2846193d45799e47e1a571c06167e497c3d6
sub-rina4 <2301100018pc@cyber-u.ac.jp>
2025-07-20 22:29:48 +0900

    add cat api

--- routes/notes.js
+++ routes/notes.js
@@ -5,7 +5,7 @@ var router = express.Router();
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = "mongodb+srv://2301100018:sansan@test.tucl0rd.mongodb.net/?retryWrites=true&w=majority&appName=test";
+const uri = "";
 const client = new MongoClient(uri);
 
 router.get('/', async (req, res) => {
