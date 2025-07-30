8713f68bf2499bc431cc26c4f57250224edab024
sub-rina4 <2301100018pc@cyber-u.ac.jp>
2025-07-30 18:41:17 +0900

    add env

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var usersRouter = require('./routes/users');
 var helloRouter = require('./routes/hello');
 var notesRouter = require('./routes/notes');
 var catRouter = require('./routes/cat');
-var qrRouter = require('./routes/qr');
+var yesnoRouter = require('./routes/yesno');
 var notes_from_bRouter = require('./routes/notes_from_b');
 
 var app = express();
@@ -20,7 +20,7 @@ app.set('view engine', 'jade');
 app.use('/hello', helloRouter);
 app.use('/notes', notesRouter);
 app.use('/cat', catRouter);
-app.use('/qr', qrRouter);
+app.use('/yesno', yesnoRouter);
 app.use('/notes_from_b', notes_from_bRouter);
 
 app.use(logger('dev'));
